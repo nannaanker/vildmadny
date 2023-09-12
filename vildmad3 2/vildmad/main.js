68fc4927a802781dff295b95d82606db83ed3f59
Jonas <joab0001@stud.kea.dk>
2023-09-12 14:28:24 +0200

    yeah2

---
+++ vildmad3 2/vildmad/main.js
@@ -0,0 +1,11 @@
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("mouseover", () => {
+    dropdown.querySelector(".dropdown-content").style.display = "block";
+  });
+
+  dropdown.addEventListener("mouseout", () => {
+    dropdown.querySelector(".dropdown-content").style.display = "none";
+  });
+});
